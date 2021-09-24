54f18b5ab872c9f96ba8d18e03d474b47405ec10
Alomerry <alomerry@hotmail.com>
2021-09-25 00:30:27 +0800

    leet-code: add 20 solution

---
+++ leet-code/1-20/13-/main.cpp
@@ -0,0 +1,43 @@
+//
+// Created by user on 8/6/20.
+//
+
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int total = 0;
+
+int romanToInt(string s) {
+    char lastCharactor = s.length()-1;
+    for (int i = s.length()-1; i >= 0; i--){
+        
+        switch (s[i]) {
+            case 'I':
+                
+                break;
+            case 'V':
+                break;
+            case 'X':
+                break;
+            case 'L':
+                break;
+            case 'C':
+                break;
+            case 'D':
+                break;
+            case 'M':
+                break;
+        }
+    }
+    return 0;
+}
+
+// MCMXCIV
+int main() {
+    return 0;
+}
