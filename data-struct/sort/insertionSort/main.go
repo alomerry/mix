e4adaf5b9431d73ba0d329219f43a5e599b44edc
Alomerry Wu <alomerry.wu@gmail.com>
2023-05-04 01:36:56 +0800

    chore: add sort

--- data-struct/sort/insertionSort/main.go
+++ data-struct/sort/insertionSort/main.go
@@ -2,22 +2,34 @@ package main
 
 import "fmt"
 
-func insertionSort(arr []int) []int {
-	var v, j int
-	for i := 0; i < len(arr); i++ {
-		v = arr[i]
-		for j = i - 1; j >= 0; j-- {
-			if v > arr[j] {
-				arr[j+1] = arr[j]
+func insertionSortMax2Min(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		for j := i; j > 0; j-- {
+			if arr[j] > arr[j-1] {
+				arr[j], arr[j-1] = arr[j-1], arr[j]
 			} else {
 				break
 			}
 		}
-		arr[j+1] = v
+	}
+	return arr
+}
+
+func insertionSortMin2Max(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		for j := i; j > 0; j-- {
+			if arr[j] < arr[j-1] {
+				arr[j], arr[j-1] = arr[j-1], arr[j]
+			} else {
+				break
+			}
+		}
 	}
 	return arr
 }
 
 func main() {
-	fmt.Println(insertionSort([]int{1, 45, 65, 12, 26, 79, 15}))
+	arr := []int{1, 45, 65, 12, 26, 79, 15}
+	fmt.Println(insertionSortMax2Min(arr))
+	fmt.Println(insertionSortMin2Max(arr))
 }
