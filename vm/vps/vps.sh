9584016a65eaea99626afba35452ee805f8db092
Alomerry Wu <alomerry.wu@gmail.com>
2024-01-08 23:28:51 +0800

    feat(vm): use shell script intall of ansible for window device

--- vm/vps/vps.sh
+++ vm/vps/vps.sh
@@ -1,59 +1,40 @@
 #!/bin/bash
 
-modules=("docs.alomerry.com" "blog.alomerry.com" "empty.alomerry.com")
+PROXY=127.0.0.1:7890
+
+GIT_RAW_URL=https://raw.githubusercontent.com
+BRANCH=master
+MIX_REPOSITORY=alomerry/mix
+MIX_VM_VPS_STATIC=vm/vps/static
+NGINX_PATH=${GIT_RAW_URL}/${MIX_REPOSITY}/${BRANCH}/${MIX_VM_VPS_STATIC}/nginx
+ACME_PATH=${GIT_RAW_URL}/${MIX_REPOSITY}/${BRANCH}/${MIX_VM_VPS_STATIC}/acme
+V2RAY_PATH=${GIT_RAW_URL}/${MIX_REPOSITY}/${BRANCH}/${MIX_VM_VPS_STATIC}/v2ray
+FRP_PATH=${GIT_RAW_URL}/${MIX_REPOSITY}/${BRANCH}/${MIX_VM_VPS_STATIC}/frp
+
 FRP_VERSION=${FRP_VERSION:-"0.51.3"}
 
-install_depandence() {
-  echo "y" | apt-get install tree \
-    aptitude \
-    ca-certificates \
-    curl \
-    gnupg \
-    wget \
-    cron \
-    lsof
-}
-
-install_nginx_depandence() {
-  echo "y" | apt-get install socat \
-    curl \
-    gnupg2 \
-    ca-certificates \
-    lsb-release \
-    ubuntu-keyring
+setup() {
+  echo "y" | apt-get install tree aptitude ca-certificates curl gnupg wget cron lsof;
+  journalctl --vacuum-time=1d && journalctl --vacuum-size=30M
 }
 
 install_nginx() {
   if [ -f /usr/sbin/nginx ]; then
-    echo "nginx has been installed."
-  else
-    install_nginx_depandence
-    curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
-    gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" | tee /etc/apt/sources.list.d/nginx.list
-    echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | tee /etc/apt/preferences.d/99nginx
-    apt update
-    apt install nginx -y
+    return;
   fi
-}
+  
+  # https://raw.githubusercontent.com/alomerry/mix/master/vm/vps/static/nginx
+  curl -fsSL $NGINX_PATH/install.sh | sh
 
-init_nginx() {
-  mkdir /root/apps/nginx/site -p
-  mkdir /root/apps/nginx/cert -p
-  mkdir /root/apps/nginx/conf -p
-  mkdir /root/apps/nginx/logs -p
+  mkdir /root/apps/nginx/{site,cert,conf,logs} -p
+  mkdir /root/apps/nginx/site/{docs,blog,empty}.alomerry.com -p
+  touch /root/apps/nginx/cert/{privkey,fullchain}.pem
 
-  touch /root/apps/nginx/cert/privkey.pem
-  touch /root/apps/nginx/cert/fullchain.pem
-
-  wget -P /etc/systemd/system/ https://raw.githubusercontent.com/alomerry/mix/master/vm/ansible/playbooks/roles/vps/files/nginx/nginx.service
-  wget -P /etc/nginx/ https://raw.githubusercontent.com/alomerry/mix/master/vm/ansible/playbooks/roles/vps/files/nginx/nginx.conf
-  wget -P /root/apps/nginx/conf/ https://raw.githubusercontent.com/alomerry/mix/master/vm/ansible/playbooks/roles/vps/files/nginx/website.conf
+  wget -P /etc/systemd/system/    $NGINX_PATH/nginx.service
+  wget -P /etc/nginx/             $NGINX_PATH/nginx.conf
+  wget -P /root/apps/nginx/conf/  $NGINX_PATH//website.conf
   chmod 644 /etc/nginx/nginx.conf
 
-  for module in "${modules[@]}"; do
-    mkdir /root/apps/nginx/site/${module}/ -p
-  done
   systemctl daemon-reload
   sleep 1
   systemctl force-reload nginx
@@ -62,15 +43,15 @@ init_nginx() {
 install_acme() {
   if [ -f /root/.acme.sh/acme.sh ]; then
     /root/.acme.sh/acme.sh --upgrade
-    echo "acme has been installed."
-  else
-    wget -P /tmp/ https://raw.githubusercontent.com/alomerry/mix/master/vm/ansible/playbooks/roles/vps/files/acme/acme.sh
-    cat /tmp/acme.sh | sh -s -- --install-online && source /root/.bashrc
-    /root/.acme.sh/acme.sh --register-account -m alomerry.wu@gmail.com
-    /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-
-    cp /tmp/acme/account.conf /root/.acme.sh/account.conf
+    return
   fi
+  
+  # https://raw.githubusercontent.com/alomerry/mix/master/vm/vps/static/acme
+  curl -fsSL $ACME_PATH/install.sh | sh -s -- --install-online && source /root/.bashrc
+  /root/.acme.sh/acme.sh --register-account -m alomerry.wu@gmail.com
+  /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+
+  wget -P /root/.acme.sh/ $ACME_PATH/account.conf
 }
 
 install_ssl() {
@@ -88,41 +69,71 @@ install_ssl() {
   fi
 }
 
-install_frps() {
-  mkdir /root/apps/frps -p
+install_v2ray() {
+  if [ -f /usr/local/bin/v2ray ]; then
+    return
+  fi
+
+  # https://raw.githubusercontent.com/alomerry/mix/master/vm/vps/static/v2ray
+  curl -fsSL $V2RAY_PATH/install.sh | sh
+
+  case "$1" in
+  client)
+    wget -P /usr/local/etc/v2ray/ $V2RAY_PATH/client.json
+    mv /usr/local/etc/v2ray/client.json /usr/local/etc/v2ray/config.json
+    ;;
+  server)
+    wget -P /usr/local/etc/v2ray/ $V2RAY_PATH/server.json
+    mv /usr/local/etc/v2ray/server.json /usr/local/etc/v2ray/config.json
+    ;;
+  esac
+
+  systemctl enable v2ray
+  systemctl start v2ray
+}
+
+install_frp() {
+  if [ -f ${FRP_VERSION}.version ]; then
+    return
+  fi
 
   wget -P /tmp/ https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz
-  tar -xf /tmp/frp_${FRP_VERSION}_linux_amd64.tar.gz --strip-components 1 -C /root/apps/frps/
 
-  # touch /root/apps/frps/${FRP_VERSION}.version
+  case "$1" in
+  client)
+    rm -rf /root/apps/frpc && mkdir /root/apps/frpc -p
+    tar -xf /tmp/frp_${FRP_VERSION}_linux_amd64.tar.gz --strip-components 1 -C /root/apps/frpc/
+    wget -P /etc/systemd/system/ $FRP_PATH/frpc.service
+    # TODO frpc.ini
+    touch /root/apps/frpc/${FRP_VERSION}.version
+    systemctl enable frpc.service && systemctl stop frpc.service
+    systemctl daemon-reload && systemctl start frpc.service
+    ;;
+  server)
+    rm -rf /root/apps/frps && mkdir /root/apps/frps -p
+    tar -xf /tmp/frp_${FRP_VERSION}_linux_amd64.tar.gz --strip-components 1 -C /root/apps/frps/
+    wget -P /etc/systemd/system/ $FRP_PATH/frps.service
+    # TODO frps.ini
+    touch /root/apps/frps/${FRP_VERSION}.version
+    rm -rf /root/apps/frps/{frps.tar.gz,frpc,frpc_full.ini,frps_full.ini,frpc.ini}
 
-  wget -P /etc/systemd/system/ https://raw.githubusercontent.com/alomerry/mix/master/vm/ansible/playbooks/roles/frps/files/frps.service
+    systemctl enable frps.service && systemctl stop frps.service
+    systemctl daemon-reload && systemctl start frps.service
+    ;;
+  esac
 
-  systemctl enable frps.service
-  systemctl stop frps.service
-  systemctl daemon-reload
-  systemctl start frps.service
-
-  # clean_frps
-  rm -rf /root/apps/frps/frps.tar.gz
-  rm -rf /root/apps/frps/frpc
-  rm -rf /root/apps/frps/frpc_full.ini
-  rm -rf /root/apps/frps/frps_full.ini
-  rm -rf /root/apps/frps/frpc.ini
+  rm -rf /tmp/frp_${FRP_VERSION}_linux_amd64.tar.gz
 }
 
 main() {
-
-  journalctl --vacuum-time=1d && journalctl --vacuum-size=30M
-
-  install_depandence
+  setup
+  
   case "$1" in
-  frps)
-    install_frps
+  frp)
+    install_frp $2
     ;;
   nginx)
     install_nginx
-    init_nginx
     ;;
   acme)
     install_acme
