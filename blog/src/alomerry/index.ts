ad729d4920f2ffd5d59b61fefbbd840992e28891
Alomerry Wu <alomerry.wu@gmail.com>
2024-02-14 20:12:18 +0800

    chore: refine waline css

--- blog/src/alomerry/index.ts
+++ blog/src/alomerry/index.ts
@@ -3,6 +3,6 @@ import dayjs from 'dayjs'
 export function formatDateByAlomerry(d: string | Date, onlyDate = true) {
   const date = dayjs(d)
   if (onlyDate || date.year() === dayjs().year())
-    return date.format('MM-D')
-  return date.format('MM-D, YYYY')
+    return date.format('MMM D')
+  return date.format('MMM D, YYYY')
 }
