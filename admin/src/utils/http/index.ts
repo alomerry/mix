7211b54675559b10c7d582f9b3d47a8866bafdf1
Alomerry Wu <alomerry.wu@gmail.com>
2024-03-17 06:37:55 +0800

    feat(admin): fix axios base url

--- admin/src/utils/http/index.ts
+++ admin/src/utils/http/index.ts
@@ -16,6 +16,7 @@ import { useUserStoreHook } from "@/store/modules/user";
 
 // 相关配置请参考：www.axios-js.com/zh-cn/docs/#axios-request-config-1
 const defaultConfig: AxiosRequestConfig = {
+  baseURL: 'https://mix-gw.alomerry.com',
   // 请求超时时间
   timeout: 360 * 1000,
   headers: {
