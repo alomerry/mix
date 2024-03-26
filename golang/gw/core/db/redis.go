6956fbb5819501d476c22cb9c20675f266ca8987
Alomerry Wu <alomerry.wu@gmail.com>
2024-03-26 22:25:40 +0800

    fix(admin): use cluster redis client

--- golang/gw/core/db/redis.go
+++ golang/gw/core/db/redis.go
@@ -3,19 +3,30 @@ package db
 import (
 	"github.com/alomerry/go-tools/static/env"
 	"github.com/redis/go-redis/v9"
+	"os"
+	"strings"
 )
 
 var (
-	rdb *redis.Client
+	rdb *redis.ClusterClient
 )
 
 func init() {
-	rdb = redis.NewClient(&redis.Options{
-		Addr:     env.GetRedisDSN(),
-		Password: env.GetRedisAK(),
+	rdb = redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs: GetRedisClusterDSN(),
+		// To route commands by latency or randomly, enable one of the following.
+		// RouteByLatency: true,
+		// RouteRandomly: true,
+		Password:            env.GetRedisAK(),
+		CredentialsProvider: nil,
 	})
 }
 
-func Redis() *redis.Client {
+func GetRedisClusterDSN() []string {
+	const REDIS_CLUSTER_DSN = "REDIS_CLUSTER_DSN"
+	return strings.Split(os.Getenv(REDIS_CLUSTER_DSN), ",")
+}
+
+func Redis() *redis.ClusterClient {
 	return rdb
 }
